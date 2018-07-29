170f0c07dddd23ae4c2d287187e1158771b91ab3
rob-now <rnowakowski.prv@gmail.com>
2018-07-29 12:21:55 +0200

    WIP: Routes

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./config')
+const users = require('./routes/api/users')
+const profile = require('./routes/api/profile')
+const posts = require('./routes/api/posts')
 
 const app = express()
 
+// Connect to database
 mongoose
   .connect(config.getDbConnectionString(), { useNewUrlParser: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.log(err.message))
 
-const port = process.env.PORT || 5000
-
 app.get('/', (req, res) => res.send('Hello!'))
+
+// Use routes
+app.use('/api/users', users)
+app.use('/api/profile', profile)
+app.use('/api/posts', posts)
+
+const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
