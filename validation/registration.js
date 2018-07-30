29b75868e781be84355be07471952e9d92484d31
rob-now <rnowakowski.prv@gmail.com>
2018-07-30 14:07:47 +0200

    Validation: Checking if name is empty

--- validation/registration.js
+++ validation/registration.js
@@ -4,8 +4,18 @@ const isEmpty = require('./isEmpty')
 module.exports = function validateRegisterInput(data) {
   const errors = {}
 
+  // If name is empty use empty string
+  // This is necessary for the Validator which validates strings only
+  data.name = !isEmpty(data.name)
+    ? data.name
+    : ''
+
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = 'Name must be between 2 and 30 characters.'
+  }
+
+  if (Validator.isEmpty(data.name)) {
+    errors.name = 'Name is required.'
   }
 
   return {
