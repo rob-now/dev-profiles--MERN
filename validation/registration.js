defb2aed07b10b0f0551f8a811b962017eeba598
rob-now <rnowakowski.prv@gmail.com>
2018-07-30 14:37:33 +0200

    All fields in register endpoint are validated

--- validation/registration.js
+++ validation/registration.js
@@ -4,11 +4,12 @@ const isEmpty = require('./isEmpty')
 module.exports = function validateRegisterInput(data) {
   const errors = {}
 
-  // If name is empty use empty string
+  // If name, email, password is empty, use empty string instead
   // This is necessary for the Validator which validates strings only
-  data.name = !isEmpty(data.name)
-    ? data.name
-    : ''
+  data.name = !isEmpty(data.name) ? data.name : ''
+  data.email = !isEmpty(data.email) ? data.email : ''
+  data.password = !isEmpty(data.password) ? data.password : ''
+  data.password2 = !isEmpty(data.password2) ? data.password2 : ''
 
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = 'Name must be between 2 and 30 characters.'
@@ -16,6 +17,30 @@ module.exports = function validateRegisterInput(data) {
 
   if (Validator.isEmpty(data.name)) {
     errors.name = 'Name is required.'
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = 'Email is required.'
+  }
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = 'Email is invalid.'
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = 'Password is required.'
+  }
+
+  if (!Validator.isLength(data.password, { min: 6, max: 32 })) {
+    errors.password = 'Password must be between 6 and 32 characters.'
+  }
+
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = 'Confirm password is required.'
+  }
+
+  if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = 'Passwords must be the same.'
   }
 
   return {
