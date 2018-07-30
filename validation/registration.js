c8c7915cfb7cd12416a74136cc48566265152ac9
rob-now <rnowakowski.prv@gmail.com>
2018-07-30 15:15:24 +0200

    Changed order of validation in register endpoint to proper one

--- validation/registration.js
+++ validation/registration.js
@@ -19,20 +19,20 @@ module.exports = function validateRegisterInput(data) {
     errors.name = 'Name is required.'
   }
 
-  if (Validator.isEmpty(data.email)) {
-    errors.email = 'Email is required.'
-  }
-
   if (!Validator.isEmail(data.email)) {
     errors.email = 'Email is invalid.'
   }
 
-  if (Validator.isEmpty(data.password)) {
-    errors.password = 'Password is required.'
+  if (Validator.isEmpty(data.email)) {
+    errors.email = 'Email is required.'
   }
 
   if (!Validator.isLength(data.password, { min: 6, max: 32 })) {
     errors.password = 'Password must be between 6 and 32 characters.'
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = 'Password is required.'
   }
 
   if (Validator.isEmpty(data.password2)) {
