d265872a497fe603871256ee13041d2878fff683
rob-now <rnowakowski.prv@gmail.com>
2018-07-31 12:31:47 +0200

    WIP: Making profile protected route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const passport = require('passport')
+// Profile model
+const Profile = require('../../models/Profile')
+// User model
+const User = require('../../models/User')
+
 
 const router = express.Router()
 
@@ -6,5 +13,20 @@ const router = express.Router()
 // @desc    Tests profile route
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Profile endpoint works' }))
+
+// @route   GET api/profile
+// @desc    Get current user profile
+// @access  Private
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const errors = {}
+
+  Profile.findOne({ user: req.user.id })
+    .then((profile) => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for such user'
+        return res.status(404).json(errors)
+      }
+    })
+})
 
 module.exports = router
