e5b1a6e9ad01f98779a025666ff10b2328d8fbf2
rob-now <rnowakowski.prv@gmail.com>
2018-07-31 12:39:01 +0200

    Profile route working and showing no profile found for now

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -26,7 +26,10 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
         errors.noprofile = 'There is no profile for such user'
         return res.status(404).json(errors)
       }
+
+      res.json(profile)
     })
+    .catch(err => res.status(404).json(err))
 })
 
 module.exports = router
