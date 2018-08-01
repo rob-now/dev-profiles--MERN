33defd09f9485f362a05841d44469dba17c0101b
rob-now <rnowakowski.prv@gmail.com>
2018-08-01 09:55:52 +0200

    Route for getting all profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -36,6 +36,24 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
     .catch(err => res.status(404).json(err))
 })
 
+// @route   GET api/profile/all
+// @desc    Get all profiles
+// @access  Public
+router.get('/all', (req, res) => {
+  const errors = {}
+
+  Profile.find()
+    .populate('user', ['name', 'avatar'])
+    .then((profiles) => {
+      if (!profiles) {
+        errors.noprofile = 'No profiles found'
+        res.status(404).json(errors)
+      }
+      res.json(profiles)
+    })
+    .catch(() => res.status(404).json({ profile: 'No profiles found' }))
+})
+
 // @route   GET api/profile/handle/:handle
 // @desc    Get profile by handle
 // @access  Public
