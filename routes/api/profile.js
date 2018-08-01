03110a512a7c34394698111e6d31f0f3390c1259
rob-now <rnowakowski.prv@gmail.com>
2018-08-01 10:13:13 +0200

    Route for adding experience to profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -158,4 +158,29 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
     })
 })
 
+// @route   POST api/profile/experience
+// @desc    Add experience to profile
+// @access  Private
+router.post('/experience', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Profile.findOne({ user: req.user.id })
+    .then((profile) => {
+      const newExperience = {
+        title: req.body.title,
+        company: req.body.company,
+        location: req.body.location,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description,
+      }
+
+      // Add to experience array at the beginning
+      profile.experience.unshift(newExperience)
+
+      // Save updated profile to database
+      // and send back updated profile to the endpoint
+      profile.save().then(profile => res.json(profile))
+    })
+})
+
 module.exports = router
