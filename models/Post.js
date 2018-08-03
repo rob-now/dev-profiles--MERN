07cc2053dd96e9ba79fa6c7bed35212b7543dd9f
rob-now <rnowakowski.prv@gmail.com>
2018-08-03 10:15:46 +0200

    Post Model created

---
+++ models/Post.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose')
+
+const { Schema } = mongoose
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  // We won't populate name and avatar from user in our posts routes
+  // We want tem to be available even if user decides to remove account and profile
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+      },
+    },
+  ],
+  comment: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+const Post = mongoose.model('post', PostSchema)
+
+module.exports = Post
