3c6d44eda19717e69cfae19a0c0c79805255122a
rob-now <rnowakowski.prv@gmail.com>
2018-08-04 11:16:41 +0200

    Setup eslint in client folder

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>My app</h1>
+        <h1>
+          My app
+        </h1>
       </div>
     );
   }
