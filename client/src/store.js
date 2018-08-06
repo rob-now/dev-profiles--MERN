ecbf10fe02dabf6b60584bce8411c32a96d8326a
rob-now <rnowakowski.prv@gmail.com>
2018-08-06 10:15:32 +0200

    Bring in rootReducer to our store

--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,9 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import rootReducer from './reducers'
+
+// Initial state of the store
+const initialState = {}
 
 // Create middleware variable which is array of different middlewares
 const middleware = [thunk]
@@ -7,8 +11,8 @@ const middleware = [thunk]
 // Template store with all 3 arguments (reducer, initial state, enhancer = middleware)
 // Use spread operator on middleware to apply all middleware from the array of middlewares
 const store = createStore(
-  () => [], // Reducer
-  {}, // Initial state
+  rootReducer, // Reducer
+  initialState, // Initial state
   applyMiddleware(...middleware), // Middleware
 )
 
