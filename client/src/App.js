098e99d9d500d1cb66c289134e8131750168a800
rob-now <rnowakowski.prv@gmail.com>
2018-08-06 14:58:47 +0200

    Keeping isAuthenticated and user state in Redux on page reload and every app link

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css'
 import { Provider } from 'react-redux'
+import jwt_decode from 'jwt-decode'
+import setAuthToken from './utils/setAuthToken'
+import { setCurrentUser } from './actions/authActions'
 import store from './store'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Landing from './components/layout/Landing'
 import Register from './components/authorization/Register'
 import Login from './components/authorization/Login'
+
+// Check for token
+if (localStorage.jwtToken) {
+  // Set token in Authorization header
+  setAuthToken(localStorage.jwtToken)
+  // Decode the token and get user info and expiration
+  const decoded = jwt_decode(localStorage.jwtToken)
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded))
+}
 
 class App extends Component {
   render() {
