123be2f8392422f5dbeacb1348353622fcf7fde6
rob-now <rnowakowski.prv@gmail.com>
2018-08-06 10:09:40 +0200

    Auth reducer

---
+++ client/reducers/authReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  isAthenticated: false,
+  user: {},
+}
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
