09284b86e1f6b2743ffa2cd32fbedbc1cb886cb6
rob-now <rnowakowski.prv@gmail.com>
2018-08-07 14:08:16 +0200

    Eslint parser on server changed to babel-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-    "extends": "airbnb",
-    "rules": {
-        "semi": 0,
-        "no-shadow": 0
-    }
-};
+  extends: 'airbnb',
+  parser: 'babel-eslint',
+  rules: {
+    semi: 0,
+    'no-shadow': 0,
+  },
+}
