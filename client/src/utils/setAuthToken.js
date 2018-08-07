ef902d88b5499988299a1ed86ed70ebc062a85b8
rob-now <rnowakowski.prv@gmail.com>
2018-08-07 13:42:54 +0200

    Fixed typo

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -6,7 +6,7 @@ const setAuthToken = (token) => {
     axios.defaults.headers.common.Authorization = token
   } else {
     // Remove Authorization header
-    delete axios.defaults.header.common.Authorization
+    delete axios.defaults.headers.common.Authorization
   }
 }
 
