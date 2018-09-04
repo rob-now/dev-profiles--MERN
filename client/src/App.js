0e4dbb067a1d13ee3f37c3b171aad42aea758cc4
rob-now <rnowakowski.prv@gmail.com>
2018-09-04 13:25:21 +0200

    Added private route to create-profile

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Login from './components/authorization/Login'
 import Dashboard from './components/dashboard/Dashboard'
 import { clearCurrentProfile } from './actions/profileActions'
 import PrivateRoute from './components/common/PrivateRoute'
+import CreateProfile from './components/create-profile/CreateProfile'
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -48,6 +49,7 @@ class App extends Component {
               <Route exact path="/login" component={Login} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
+                <PrivateRoute exact path="/create-profile" component={CreateProfile} />
               </Switch>
             </div>
             <Footer />
