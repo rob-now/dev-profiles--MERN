5d6e3223d52453847a2745848a02dc232948f633
rob-now <rnowakowski.prv@gmail.com>
2018-09-04 12:58:29 +0200

    Private route ready - with PropTypes

--- client/src/components/common/PrivateRoute.js
+++ client/src/components/common/PrivateRoute.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 
 const PrivateRoute = ({ component: Component, auth, ...rest }) => (
   <Route
@@ -9,6 +10,10 @@ const PrivateRoute = ({ component: Component, auth, ...rest }) => (
     }
   />
 )
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired,
+}
 
 const mapStateToProps = state => ({
   auth: state.auth,
