f6db0934002391bbf19b277ba3fd9f7475ad333c
rob-now <rnowakowski.prv@gmail.com>
2018-09-04 13:21:39 +0200

    WIP: Preparing CreateProfile component - initial jsx

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -21,7 +21,19 @@ class CreateProfile extends Component {
   }
 
   render() {
-    return <div />
+    return (
+      <div className="create-profile">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Create profile</h1>
+              <p className="lead text-center">Fill out the form to create your profile.</p>
+              <small className="d-block pb-3">* = required fields</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
