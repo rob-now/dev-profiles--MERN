3195cc712de591687ae4e1f92f511ecd2f8185a5
rob-now <rnowakowski.prv@gmail.com>
2018-09-04 13:36:57 +0200

    Added private route to create-profile

--- client/src/App.js
+++ client/src/App.js
@@ -49,6 +49,8 @@ class App extends Component {
               <Route exact path="/login" component={Login} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
                 <PrivateRoute exact path="/create-profile" component={CreateProfile} />
               </Switch>
             </div>
