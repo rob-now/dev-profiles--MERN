ab2ec6b68db52b027e84c301f660b2cd74de7d0f
rob-now <rnowakowski.prv@gmail.com>
2018-09-13 11:02:36 +0200

    Buttons for edit profile, add experience and education

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import { getCurrentProfile } from '../../actions/profileActions'
 import Spinner from '../common/Spinner'
+import ProfileActions from './ProfileActions'
 
 class Dashboard extends Component {
   componentDidMount() {
@@ -25,6 +26,7 @@ class Dashboard extends Component {
           <p className="lead text-muted">
             Welcome <Link to={`/profile/${profile.handle}`}>{user.name}</Link>
           </p>
+          <ProfileActions />
         </div>
       )
     } else {
