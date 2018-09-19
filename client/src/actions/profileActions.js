63519dccc7f2da504114681e44cd593c2527861e
rob-now <rnowakowski.prv@gmail.com>
2018-09-19 09:45:07 +0200

    deleteExperience action

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -64,6 +64,20 @@ export const addEducation = (eduData, history) => (dispatch) => {
     }))
 }
 
+// Delete experience
+export const deleteExperience = expId => (dispatch) => {
+  axios
+    .delete(`/api/profile/experience/${expId}`)
+    .then(res => dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    }))
+    .catch(err => dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data,
+    }))
+}
+
 // Delete account and profile
 export const deleteAccount = () => (dispatch) => {
   if (window.confirm('Are you sure? This cannot be undone!')) {
