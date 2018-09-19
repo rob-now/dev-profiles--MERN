d2ab8251649fe506d659017c8500a19f412938e6
rob-now <rnowakowski.prv@gmail.com>
2018-09-19 09:14:22 +0200

    Displaying experience table

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 import { getCurrentProfile, deleteAccount } from '../../actions/profileActions'
 import Spinner from '../common/Spinner'
 import ProfileActions from './ProfileActions'
+import Experience from './Experience'
 
 class Dashboard extends Component {
   componentDidMount() {
@@ -31,6 +32,7 @@ class Dashboard extends Component {
             Welcome <Link to={`/profile/${profile.handle}`}>{user.name}</Link>
           </p>
           <ProfileActions />
+          <Experience experience={profile.experience} />
           <div style={{ marginBottom: '60px' }} />
           <button type="button" className="btn btn-danger" onClick={this.handleDeleteAccount}>
             Delete account
