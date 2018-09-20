f20a6d66ef9e72037dc48328b76d93561a5b85c6
rob-now <rnowakowski.prv@gmail.com>
2018-09-20 10:12:08 +0200

    Profile component with sub-components

---
+++ client/src/components/profile/ProfileGitHub.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ProfileGitHub extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Profile GitHub</h1>
+      </div>
+    )
+  }
+}
+
+export default ProfileGitHub
