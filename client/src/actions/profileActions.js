48fa1c2fefd9b13baf821ef7c9c3c46fc9c18182
rob-now <rnowakowski.prv@gmail.com>
2018-09-20 09:11:12 +0200

    getProfiles action

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import {
   GET_PROFILE,
+  GET_PROFILES,
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
   GET_ERRORS,
@@ -89,6 +90,21 @@ export const deleteEducation = eduId => (dispatch) => {
     .catch(err => dispatch({
       type: GET_ERRORS,
       payload: err.response.data,
+    }))
+}
+
+// Get all profiles
+export const getProfiles = () => (dispatch) => {
+  dispatch(setProfileLoading())
+  axios
+    .get('/api/profile/all')
+    .then(res => dispatch({
+      type: GET_PROFILES,
+      payload: res.data,
+    }))
+    .catch(() => dispatch({
+      type: GET_PROFILES,
+      payload: null,
     }))
 }
 
