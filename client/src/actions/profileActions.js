6fe05dfa6190b255017f4bd07feb80147db35c09
rob-now <rnowakowski.prv@gmail.com>
2018-09-20 10:10:38 +0200

    getProfileByHandle action

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -32,6 +32,21 @@ export const getCurrentProfile = () => (dispatch) => {
     }))
 }
 
+// GET profile by handle
+export const getProfileByHandle = handle => (dispatch) => {
+  dispatch(setProfileLoading())
+  axios
+    .get(`/api/profile/handle/${handle}`)
+    .then(res => dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    }))
+    .catch(() => dispatch({
+      type: GET_PROFILE,
+      payload: null,
+    }))
+}
+
 // Create profile
 export const createProfile = (profileData, history) => (dispatch) => {
   axios
