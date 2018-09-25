169ee5e6e245e23e0e5150287981a19cc879dce7
rob-now <rnowakowski.prv@gmail.com>
2018-09-25 10:57:59 +0200

    Get posts action

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-import { ADD_POST, GET_ERRORS } from './types'
+import {
+  ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING,
+} from './types'
 
 // Add post
 export const addPost = postData => (dispatch) => {
@@ -13,5 +15,25 @@ export const addPost = postData => (dispatch) => {
     .catch(err => dispatch({
       type: GET_ERRORS,
       payload: err.response.data,
+    }))
+}
+
+// Set loading state
+export const setPostLoading = () => ({
+  type: POST_LOADING,
+})
+
+// Get posts
+export const getPosts = () => (dispatch) => {
+  dispatch(setPostLoading())
+  axios
+    .get('/api/posts')
+    .then(res => dispatch({
+      type: GET_POSTS,
+      payload: res.data,
+    }))
+    .catch(() => dispatch({
+      type: GET_POSTS,
+      payload: null,
     }))
 }
