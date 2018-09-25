44260da87e2a6dbea0ba5f33673947a710b25727
rob-now <rnowakowski.prv@gmail.com>
2018-09-25 10:10:03 +0200

    Routing to Posts component

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,7 @@ import AddEducation from './components/add-credentials/AddEducation'
 import Profiles from './components/profiles/Profiles'
 import Profile from './components/profile/Profile'
 import NotFound from './components/not-found/NotFound'
+import Posts from './components/posts/Posts';
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -69,6 +70,9 @@ class App extends Component {
               </Switch>
               <Switch>
                 <PrivateRoute exact path="/add-education" component={AddEducation} />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts} />
               </Switch>
               <Route exact path="/not-found" component={NotFound} />
             </div>
