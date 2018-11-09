ed670b90d092ee42b09254b569aee840c25e2889
rob-now <rnowakowski.prv@gmail.com>
2018-11-09 12:10:03 +0100

    Like and unlike thumbs color

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -19,6 +19,15 @@ class PostItem extends Component {
     this.props.removeLike(id)
   }
 
+  findUserLike = (likes) => {
+    const { auth } = this.props
+    if (likes.filter(like => like.user === auth.user.id).length > 0) {
+      return true
+    } else {
+      return false
+    }
+  }
+
   render() {
     const { post, auth } = this.props
 
@@ -27,11 +36,13 @@ class PostItem extends Component {
         <div className="row">
           <div className="col-md-2">
             <a href="profile.html">
-              <img className="rounded-circle d-none d-md-block"
-                   src={post.avatar}
-                   alt=""/>
+              <img
+                className="rounded-circle d-none d-md-block"
+                src={post.avatar}
+                alt=""
+              />
             </a>
-            <br/>
+            <br />
             <p className="text-center">{post.name}</p>
           </div>
           <div className="col-md-10">
@@ -41,7 +52,10 @@ class PostItem extends Component {
               className="btn btn-light mr-1"
               onClick={() => this.handlePostLike(post._id)}
             >
-              <i className="text-info fas fa-thumbs-up"></i>
+              <i className={classnames('fas fa-thumbs-up', {
+                'text-info': this.findUserLike(post.likes),
+              })}
+              />
               <span className="badge badge-light">{post.likes.length}</span>
             </button>
             <button
@@ -49,7 +63,7 @@ class PostItem extends Component {
               className="btn btn-light mr-1"
               onClick={() => this.handlePostUnlike(post._id)}
             >
-              <i className="text-secondary fas fa-thumbs-down"></i>
+              <i className="text-secondary fas fa-thumbs-down" />
             </button>
             <Link to={`/post/${post._id}`} className="btn btn-info mr-1">
               Comments
