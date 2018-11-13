43be4ea4544d2933bfe699653ba70de05ec1a03f
rob-now <rnowakowski.prv@gmail.com>
2018-11-13 13:29:58 +0100

    Prepare to deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const passport = require('passport')
+const path = require('path')
 const config = require('./config')
 const users = require('./routes/api/users')
 const profile = require('./routes/api/profile')
@@ -29,6 +30,17 @@ require('./config/passport')(passport)
 app.use('/api/users', users)
 app.use('/api/profile', profile)
 app.use('/api/posts', posts)
+
+// Server static assests if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'))
+
+  // Load index.html for any route
+  app.get('*', (req, res) => {
+    res.sendFile(path(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const port = process.env.PORT || 5000
 
